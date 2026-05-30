c8b1db6d95a41a36e73fef068160575ba120a39e
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-05-30 22:01:52 +0800

    use Algolia DocSearch

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -86,38 +86,56 @@ export default defineConfig({
     editLink: true,
     aside: true,
     search: {
-      provider: "local",
+      provider: 'algolia',
       options: {
-        translations: {
-          button: { buttonText: "搜索", buttonAriaLabel: "搜索" },
-          modal: {
-            noResultsText: "无法找到相关结果",
-            resetButtonTitle: "清除查询条件",
-            footer: {
-              selectText: "选择",
-              navigateText: "切换",
-              closeText: "关闭",
-            },
-          },
-        },
-        locales: {
-          tw: {
-            translations: {
-              button: { buttonText: "搜索", buttonAriaLabel: "搜索" },
-              modal: {
-                noResultsText: "無法找到相關結果",
-                resetButtonTitle: "清除查詢條件",
-                footer: {
-                  selectText: "選擇",
-                  navigateText: "切換",
-                  closeText: "關閉",
-                },
-              },
-            },
-          },
-        },
+        appId: 'GCI0MKFKYA',
+        apiKey: '6b86778438eaf70ca177b9652d982c47',
+        indexName: 'vmct_cn_top_gci0mkfkya_pages'
       },
-    },
+      locales: {
+        zh: {
+          placeholder: '搜索',
+          translations: {
+            button: {
+              buttonText: '搜索',
+              buttonAriaLabel: '搜索'
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消'
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除'
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接'
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者'
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈'
+              }
+            }
+          }
+        }
+      }
+    }
   },
   rewrites: { "zh/:splat(.*)": ":splat" },
   locales: {
